573caa8d3ce13a725d2671e589438646b0a07203
Thomas F. Abraham <tfabraham@users.noreply.github.com>
2017-08-13 17:47:25 -0500

    Add logos for VS Marketplace; update 7-Zip; fix build script

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -24,5 +24,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.97.0.0")]
-[assembly: AssemblyFileVersion("1.97.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
